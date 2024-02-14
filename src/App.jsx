8ce1ddb4b78508525520c66151cf7f4d054deef6
Megan Grant <granmega@oregonstate.edu>
2024-02-13 21:48:01 -0800

    figuring stuff out

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,62 @@
+import {
+    Link,
+    NavLink,
+    Outlet,
+    useParams,
+    userSearchParams,
+    useRouteError
+} from 'react-router-dom'
+
+export function Home() {
+    return (
+        <>
+            <h1>Home</h1>
+            <p><Link to="/people">People</Link></p>
+            <p><Link to="/planets">Planets</Link></p>
+            <p><Link to="/films">Films</Link></p>
+        </>
+    )
+}
+
+export function People() {
+    return <h1>People</h1>
+}
+
+export function Planets() {
+    return <h1>Planets</h1>
+}
+
+export function Films() {
+    return <h1>Films</h1>
+}
+
+export function Root(props) {
+    const { children } = props
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/people">People</NavLink></li>
+                    <li><NavLink to="/planets">Planets</NavLink></li>
+                    <li><NavLink to="/films">Films</NavLink></li>
+                </ul>
+            </nav>
+            <main>{children || <Outlet />}</main>
+        </>
+    )
+}
+
+export function ErrorPage() {
+    const error = useRouteError()
+    console.error(error)
+    return (
+        <>
+            <h1>Error</h1>
+            <p>{error.statusText || error.message}</p>
+        </>
+    )
+}
 export default function App() {
     return (
         <h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
 
+import {
+    Home,
+    People,
+    Films,
+    Planets, 
+    ErrorPage
+} from './App'
 import './index.css'
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <Root><ErrorPage /></Root>,
+        children: [
+            { index: true, element: <Home /> },
+            { path: "people", element: <People /> },
+            { path: "planets", element: <Planets /> },
+            { path: "films", element: <Films /> }
+        ]
+    },
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
+        {/* <App /> */}
     </React.StrictMode>,
 )
